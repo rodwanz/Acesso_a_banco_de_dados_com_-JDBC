e92eea82eb9882561916e91db377fd28ea96c0b8
Rodrigo Wanzeler <wanzervai@gmail.com>
2022-02-02 12:42:38 -0300

    JDBC3

--- src/application/ProgramJDBC.java
+++ src/application/ProgramJDBC.java
@@ -1,9 +1,12 @@
 package application;
 
 import java.sql.Connection;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 
 import db.DB;
 
@@ -11,24 +14,53 @@ public class ProgramJDBC {
 
 	public static void main(String[] args) {
 		
+		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
 		Connection conn = null;
-		Statement st = null;
-		ResultSet rs = null;
+		PreparedStatement st = null;
 		
 		try {
 			conn = DB.getConnection();
-			st = conn.createStatement();
-			rs = st.executeQuery("select * from department");
+			//EXAMPLE 1
+
+			st = conn.prepareStatement(
+					"INSERT INTO seller "
+					+ "(Name, Email, Birthdate, BaseSalary, DepartmentId) "
+					+ "VALUES "
+					+ "(?, ?, ?, ?, ?)",
+					Statement.RETURN_GENERATED_KEYS);
 			
-			while(rs.next()) {
-				System.out.println(rs.getInt("Id") + ", " + rs.getString("Name"));
+			st.setString(1, "Richard Humberguer");
+			st.setString(2, "carl@gmail.com");
+			st.setDate(3, new java.sql.Date(sdf.parse("22/04/1983").getTime()));
+			st.setDouble(4, 3000.0);
+			st.setInt(5, 5);
+
+			/*EXAMPLE 2
+			st = conn.prepareStatement(
+					"insert into department (Name) values ('D1'), ('D2')",
+					Statement.RETURN_GENERATED_KEYS);
+			*/
+			
+			int rowsAffected = st.executeUpdate();
+			
+			if(rowsAffected > 0) {
+				ResultSet rs = st.getGeneratedKeys();
+				while(rs.next()) {
+					int id = rs.getInt(1);
+					System.out.println("Done! Id: " + id);
+				}
 			}
+			else {
+				System.out.println("No rows affected!");
+			}			
 		}
 		catch(SQLException e) {
 			e.printStackTrace();
 		}
-		finally{
-			DB.closeResultset(rs);
+		catch(ParseException e) {
+			e.printStackTrace();
+		}
+		finally {
 			DB.closeStatement(st);
 			DB.closeConnection();
 		}
